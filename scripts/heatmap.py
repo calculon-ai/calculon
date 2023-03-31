67db512f969485b71c4276c89f5704fef12a3ba2
Nic McDonald <n.mcdonald83@gmail.com>
2023-03-31 15:08:19 -0600

    Update 3dplot, add heatmap

--- scripts/heatmap.py
+++ scripts/heatmap.py
@@ -3,10 +3,11 @@
 import argparse
 import json
 import matplotlib
-matplotlib.use('TkAgg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tkr
 import numpy as np
+import tol_colors as tc
 
 
 def main(args):
@@ -32,46 +33,52 @@ def main(args):
   assert len(pps) > 1, f'len(pps)={len(pps)} can\'t plot'
 
   # Gathers data
-  fdata = np.full((len(pps), len(tps)), float('NaN'))
+  fdata = np.full((len(tps), len(pps)), float('NaN'))
   for tp in data.keys():
     for pp in data[tp].keys():
       if 'stats' in data[tp][pp]:
         v = data[tp][pp]['stats']['sample_rate']
-        fdata[pps.index(pp)][tps.index(tp)] = v
+        fdata[tps.index(tp)][pps.index(pp)] = v
         print(f'{tp},{pp} is {v}')
       else:
         print(f'{tp},{pp} has none')
 
+  # Determines range
+  minf = min(map(min, fdata))
+  maxf = max(map(max, fdata))
+  black_threshold = minf + (maxf - minf) * 0.30
+  print(f'min={minf} max={maxf} thres={black_threshold}')
+
+  # Creates the plot
   fig = plt.figure()
-  ax = fig.add_subplot(111, projection='3d')
-  X, Y = np.meshgrid(list(range(len(tps))), list(range(len(pps))))
-  ax.plot_surface(X, Y, fdata, rstride=1, cstride=1,
-                  cmap='rainbow',
-                  edgecolor='none')
-  ax.set_xlabel('Tensor Parallelism')
-  ax.set_ylabel('Pipeline Parallelism')
-  ax.set_zlabel('Sample Rate (s/sec)')
+  ax = fig.add_subplot(1, 1, 1)
+  ax.imshow(fdata, origin='lower', cmap='hot')#, linewidth=0.5)
+  ax.set_xticks(np.arange(len(pps)), labels=pps)
+  ax.set_xlabel('Pipeline Parallelism')
+  ax.set_yticks(np.arange(len(tps)), labels=tps)
+  ax.set_ylabel('Tensor Parallelism')
+  for tp in tps:
+    for pp in pps:
+      perf = fdata[tps.index(tp), pps.index(pp)]
+      color = 'black' if perf > black_threshold else 'white'
+      perf = f'{perf:.1f}'
+      text = f'{perf}'
+      ax.text(pps.index(pp), tps.index(tp), text, ha='center', va='center',
+              color=color)
   if args.title:
     ax.set_title(args.title)
-  ax.view_init(20, 180+25)
-  @tkr.FuncFormatter
-  def formatter(x, pos):
-    d = 2**x
-    if d < 1:
-      return 'duh'
-    else:
-      return str(int(d))
-  ax.xaxis.set_major_formatter(formatter)
-  ax.yaxis.set_major_formatter(formatter)
+  print(f'writing {args.output}')
   fig.tight_layout()
-  plt.show()
-
+  fig.savefig(args.output)
+  plt.close(fig)
 
 
 if __name__ == '__main__':
   ap = argparse.ArgumentParser()
   ap.add_argument('stats', type=str,
                   help='File path to stats input')
+  ap.add_argument('output', type=str,
+                  help='Output plot file')
   ap.add_argument('-t', '--title', type=str, default=None,
                   help='Title of plot')
   args = ap.parse_args()
